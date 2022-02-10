35b4b88d07a52a3452f89f16c3f1f18318104da0
Johannes Tegnér <johannes@jitesoft.com>
2022-02-10 14:01:12 +0100

    Implementation of Organisationsnummer spec v1.1.0
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

---
+++ Organisationsnummer/OrganisationsnummerException.cs
@@ -0,0 +1,6 @@
+﻿namespace Organisationsnummer;
+
+public class OrganisationsnummerException : Exception
+{
+    public OrganisationsnummerException(string message = "Invalid Swedish organization number", Exception? inner = null) : base(message: message, innerException: inner) { }
+}
